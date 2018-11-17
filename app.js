4adaf0173c5de51acc438d514c51f419eca7df16
squishy cow <lots2blox@gmail.com>
2018-11-17 07:49:38 -0800

    users can now login to vidjot thanks to passport.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const exphbs  = require('express-handlebars');
-const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// Load Routes
+// Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -28,11 +32,11 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-// Body-parser Midleware
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Static folder
+// Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Method Override Middleware
@@ -45,17 +49,20 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// Passport.js Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
-
 // Global Variables
-
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
-})
+});
 
 // Index Route
 app.get('/', (req, res) => {
@@ -68,7 +75,8 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
-  });
+});
+
 
 // Use routes
 app.use('/ideas', ideas);
--- routes/ideas.js
+++ routes/ideas.js
@@ -8,91 +8,88 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', (req, res) => {
-Idea.find({})
-    .sort({date: 'desc'})
+  Idea.find({})
+    .sort({date:'desc'})
     .then(ideas => {
-    res.render('ideas/index', {
+      res.render('ideas/index', {
         ideas:ideas
+      });
     });
-    });
-
 });
 
 // Add Idea Form
 router.get('/add', (req, res) => {
-res.render('/ideas/add');
+  res.render('ideas/add');
 });
 
 // Edit Idea Form
 router.get('/edit/:id', (req, res) => {
-Idea.findOne({
+  Idea.findOne({
     _id: req.params.id
-})
-.then(idea => {
+  })
+  .then(idea => {
     res.render('ideas/edit', {
-    idea:idea
+      idea:idea
     });
-});
+  });
 });
 
 // Process Form
 router.post('/', (req, res) => {
-let errors = [];
+  let errors = [];
 
-if(!req.body.title){
-    errors.push({text: 'Please add a title.'});
-}
-if(!req.body.details){
-    errors.push({text: 'Please add some details.'});
-}
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
 
-if(errors.length > 0){
+  if(errors.length > 0){
     res.render('/add', {
-    errors: errors,
-    title: req.body.title,
-    details: req.body.details
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
     });
-} else {
+  } else {
     const newUser = {
-    title: req.body.title,
-    details: req.body.details
+      title: req.body.title,
+      details: req.body.details
     }
     new Idea(newUser)
-    .save()
-    .then(idea => {
+      .save()
+      .then(idea => {
+        req.flash('success_msg', 'Video idea added');
         res.redirect('/ideas');
-    })
-}
+      })
+  }
 });
 
-// Edit Form Process
-
+// Edit Form process
 router.put('/:id', (req, res) => {
-Idea.findOne({
+  Idea.findOne({
     _id: req.params.id
-})
-.then(idea => {
+  })
+  .then(idea => {
     // new values
-    idea.title = req.body.title,
+    idea.title = req.body.title;
     idea.details = req.body.details;
 
     idea.save()
-    .then(idea => {
+      .then(idea => {
         req.flash('success_msg', 'Video idea updated');
         res.redirect('/ideas');
-    })
-})
+      })
+  });
 });
 
 // Delete Idea
 router.delete('/:id', (req, res) => {
-Idea.remove({_id: req.params.id})
+  Idea.remove({_id: req.params.id})
     .then(() => {
-    req.flash('success_msg', 'Video idea removed')
-    res.redirect('/ideas')
-    })
+      req.flash('success_msg', 'Video idea removed');
+      res.redirect('/ideas');
+    });
 });
-
-
 
 module.exports = router;
