7e2ee70fcd435c4288de2663d91765d251bafbb7
squishy cow <lots2blox@gmail.com>
2018-11-17 01:05:48 -0800

    added user registration with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
 const router = express.Router();
+
+// Load User Model
+require('../models/User');
+const User = mongoose.model('users');
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -16,7 +22,7 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
 let errors = [];
 
-if(req.body.password != req.bodypassword2){
+if(req.body.password != req.body.password2){
     errors.push({text: 'Passwords do not match.'});
 }
 
@@ -33,7 +39,35 @@ if(errors.length > 0){
         password2: req.body.password2
     });
 }   else {
-    res.send('passed');
+        User.findOne({email: req.body.email})
+            .then(user => {
+                if(user){
+                    req.flash('error_msg', 'Email already registered');
+                    res.redirect('/users/register');
+                } else {
+                    const newUser = new User ({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                            .then(user => {
+                                req.flash('success_msg', 'You are now registered and can log in');
+                                res.redirect('/users/login');
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                return;
+                            });
+                        });
+                    });
+                }
+            });
 }
 });
   
